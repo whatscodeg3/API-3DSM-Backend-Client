c07da4f0f2857c6e8ecaa965e185fee1483006f1
ThiagoOAL <thiago-leite01@hotmail.com>
2023-03-15 07:51:22 -0300

    feat - cadastro de clientes. Finalizado método que permite atualização de alguns atributos do cliente

--- src/main/java/com/br/WhatsCodeClientMicroservice/dto/ClientDto.java
+++ src/main/java/com/br/WhatsCodeClientMicroservice/dto/ClientDto.java
@@ -2,10 +2,13 @@ package com.br.WhatsCodeClientMicroservice.dto;
 
 import com.br.WhatsCodeClientMicroservice.models.Address;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.Date;
 
+
+@Data
 public class ClientDto {
 
     private String fullName;
