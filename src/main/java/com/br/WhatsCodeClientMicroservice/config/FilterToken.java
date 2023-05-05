95c98fe5ed2e777e5e826a73831a6201fed2a7e9
ThiagoOAL <thiago-leite01@hotmail.com>
2023-05-05 08:28:25 -0300

    descomentando a parte de autenticação

--- src/main/java/com/br/WhatsCodeClientMicroservice/config/FilterToken.java
+++ src/main/java/com/br/WhatsCodeClientMicroservice/config/FilterToken.java
@@ -1,46 +1,46 @@
-//package com.br.WhatsCodeClientMicroservice.config;
-//
-//
-//import com.br.WhatsCodeClientMicroservice.repository.UserRepository;
-//import com.br.WhatsCodeClientMicroservice.service.TokenService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//import java.io.IOException;
-//import jakarta.servlet.*;
-//import jakarta.servlet.http.*;
-//@Component
-//public class FilterToken extends OncePerRequestFilter {
-//
-//    @Autowired
-//    private TokenService tokenService;
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request,
-//                                    HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//
-//        String token;
-//
-//        var authorizationHeader = request.getHeader("Authorization");
-//
-//        if(authorizationHeader != null) {
-//            token = authorizationHeader.replace("Bearer ", "");
-//            var subject = this.tokenService.getSubject(token);
-//
-//            var user = this.userRepository.findByLogin(subject);
-//
-//            var authentication = new UsernamePasswordAuthenticationToken(user,
-//                    null, user.getAuthorities());
-//
-//            SecurityContextHolder.getContext().setAuthentication(authentication);
-//        }
-//
-//        filterChain.doFilter(request, response);
-//
-//    }
-//}
+package com.br.WhatsCodeClientMicroservice.config;
+
+
+import com.br.WhatsCodeClientMicroservice.repository.UserRepository;
+import com.br.WhatsCodeClientMicroservice.service.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+@Component
+public class FilterToken extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token;
+
+        var authorizationHeader = request.getHeader("Authorization");
+
+        if(authorizationHeader != null) {
+            token = authorizationHeader.replace("Bearer ", "");
+            var subject = this.tokenService.getSubject(token);
+
+            var user = this.userRepository.findByLogin(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user,
+                    null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+}
